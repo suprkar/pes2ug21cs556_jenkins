478082339e9bc9aed874b5d7b935fe513fec0ea2
suprkar <115251486+suprkar@users.noreply.github.com>
2024-03-09 23:08:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built\n";
   cout << "PES2UG21CS556";
   return 0;
 }
